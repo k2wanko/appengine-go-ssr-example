402afb36defa683fb1753b3942b8d1e3f46a2fd7
Kazuhiro Kubota <k2.wanko@gmail.com>
2016-12-27 20:57:26 +0900

    Add webpack-dev-server

--- build/webpack.config.js
+++ build/webpack.config.js
@@ -3,47 +3,6 @@ var webpack = require('webpack')
 
 var ExtractTextPlugin = require("extract-text-webpack-plugin")
 
-var webpackModule = {
-  rules: [
-    {
-      test: /\.vue$/,
-      loader: 'vue',
-      options: {
-        loaders: {
-          css: ExtractTextPlugin.extract({
-            loader: 'css-loader',
-            fallbackLoader: 'vue-style-loader'
-          })
-        }
-      }
-    },
-    {
-      test: /\.js$/,
-      loader: 'babel',
-      exclude: /node_modules/
-    },
-    {
-      test: /\.(png|jpg|gif|svg)$/,
-      loader: 'file',
-      options: {
-        name: '[name].[ext]?[hash]'
-      }
-    },
-    {
-      test: /\.json$/,
-      loader: 'json'
-    },
-    {
-      test: /\.html$/,
-      loader: 'html',
-      query: {
-        minimize: process.env.NODE_ENV === 'production',
-        removeAttributeQuotes: false,
-      }
-    }
-  ]
-}
-
 const client = {
   entry: './src/client.js',
   output: {
@@ -51,7 +10,46 @@ const client = {
     publicPath: '/',
     filename: 'build.js'
   },
-  module: webpackModule,
+  module: {
+    rules: [
+      {
+        test: /\.vue$/,
+        loader: 'vue',
+        options: {
+          loaders: {
+            css: ExtractTextPlugin.extract({
+              loader: 'css-loader',
+              fallbackLoader: 'vue-style-loader'
+            })
+          }
+        }
+      },
+      {
+        test: /\.js$/,
+        loader: 'babel',
+        exclude: /node_modules/
+      },
+      {
+        test: /\.(png|jpg|gif|svg)$/,
+        loader: 'file',
+        options: {
+          name: '[name].[ext]?[hash]'
+        }
+      },
+      {
+        test: /\.json$/,
+        loader: 'json'
+      },
+      {
+        test: /\.html$/,
+        loader: 'html',
+        query: {
+          minimize: process.env.NODE_ENV === 'production',
+          removeAttributeQuotes: false,
+        }
+      }
+    ]
+  },
   resolve: {
     alias: {
       'vue$': 'vue/dist/vue',
@@ -70,7 +68,8 @@ const client = {
 }
 
 var NodeSourcePlugin = require('webpack/lib/node/NodeSourcePlugin')
-webpackModule.rules[1].exclude = null
+serverModule = Object.assign({}, client.module)
+delete (serverModule.rules[1].exclude)
 
 var binding = process.binding
 process.binding = function (name) {
@@ -99,7 +98,7 @@ var server = Object.assign({}, client, {
         __dirname: 'mock'
       }),
   ]),
-  module: webpackModule,
+  module: serverModule,
 })
 
 if (process.env.NODE_ENV === 'production') {
