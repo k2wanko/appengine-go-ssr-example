22553d158be11f4f0d8120a2e2bb74acf0719ab8
Kazuhiro Kubota <k2.wanko@gmail.com>
2017-01-10 22:56:21 +0900

    Implement __INITIAL_STATE__

--- src/client.js
+++ src/client.js
@@ -1,6 +1,5 @@
 import { app, store } from './app'
 
-// TODO
-// store.replaceState(window.__INITIAL_STATE__)
+store.replaceState(global.__INITIAL_STATE__)
 
 app.$mount("#app")
--- src/server.js
+++ src/server.js
@@ -1,6 +1,7 @@
-import { app, router } from './app'
+import { app, router, store } from './app'
 import Vue from 'vue'
 import { createRenderer } from 'vue-server-renderer'
+import serialize from 'serialize-javascript'
 import layout from './index.html'
 
 Vue.config.devtools = false
@@ -18,8 +19,8 @@ const renderer = createRenderer({
     cache: global['__ComponentCache__'],
 })
 
-module.exports = function ({ res, url }) {
-// export function renderStream({ url, res }) {
+module.exports = function (context) {
+    const {url, res} = context
     router.push(url)
 
     const stream = renderer.renderToStream(app)
@@ -30,6 +31,11 @@ module.exports = function ({ res, url }) {
         res.write(chunk)
     })
     stream.on('end', () => {
+        res.write(
+            `<script>window.__INITIAL_STATE__=${
+            serialize(store.state, { isJSON: true })
+            }</script>`
+        )
         res.end(html.tail)
     })
     stream.on('error', err => {
